cab3da299fd01fb10d3019b4d5bcddacaa1ad3cd
Santiago-dev12 <moises.gonzret@gmail.com>
2025-12-09 11:39:09 -0600

    feat: se egrego el inicio de sesión conectado con el back

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-document.getElementById("loginForm").addEventListener("submit", e => {
-  e.preventDefault();
 
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
-  const role = document.getElementById("role").value;
-
-  if (!user || !pass || !role) {
-    alert("Completa todos los campos");
-    return;
-  }
-
-  const routes = {
-    reception: "./pages/reception.html",
-    maid: "./pages/maid.html"
-  };
-
-  window.location.href = routes[role];
-});
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js")
